2cb11f66658d75abc653fc31bb12baa5e5da378a
Esteban S <esteban.soubiran@insa-cvl.fr>
2021-04-25 12:00:28 +0200

    feat: add fields for user validator

--- app/Validators/UserValidator.ts
+++ app/Validators/UserValidator.ts
@@ -1,9 +1,10 @@
-import { schema } from '@ioc:Adonis/Core/Validator'
+import { schema, rules } from '@ioc:Adonis/Core/Validator'
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class UserValidator {
   constructor(protected ctx: HttpContextContract) {}
 
+  public date = new Date()
   /*
    * Define schema to validate the "shape", "type", "formatting" and "integrity" of data.
    *
@@ -26,6 +27,14 @@ export default class UserValidator {
   public schema = schema.create({
     lastName: schema.string.optional({ trim: true }),
     firstName: schema.string.optional({ trim: true }),
+    text: schema.string.optional({ trim: true }),
+    mobile: schema.string.optional({ trim: true }, [rules.mobile({ locales: ['fr-FR'] })]),
+    skills: schema.array.optional().members(schema.string({ trim: true })),
+    focusInterest: schema.array.optional().members(schema.string({ trim: true })),
+    /*
+     * We prevent user to provide random year
+     */
+    graduationYear: schema.number.optional([rules.range(1957, this.date.getFullYear() + 5)]),
   })
 
   /**
@@ -39,5 +48,12 @@ export default class UserValidator {
    * }
    *
    */
-  public messages = {}
+  public messages = {
+    'graduationYear.range': "Cette année de diplomation n'est pas acceptée",
+    'mobile.mobile': "Ce numéro de téléphone n'est pas valide",
+    'skills.array': "Le type n'est pas le bon",
+    'skills.*.string': 'Les données doivent être des chaînes de caractères',
+    'focusInterest.array': "Le type n'est pas le bon",
+    'focusInterest.*.string': 'Les données doivent être des chaînes de caractères',
+  }
 }

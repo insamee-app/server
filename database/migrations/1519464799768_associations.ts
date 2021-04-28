8804a653bd12dec0e7aca5d4a6c27f9512d6b854
Esteban S <esteban.soubiran@insa-cvl.fr>
2021-04-28 10:22:12 +0200

    fix: remove unique for associations

--- database/migrations/1519464799768_associations.ts
+++ database/migrations/1519464799768_associations.ts
@@ -6,7 +6,7 @@ export default class Associations extends BaseSchema {
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
-      table.string('name', 255).notNullable().unique()
+      table.string('name', 255).notNullable()
       table.integer('school_id').unsigned().notNullable().references('id').inTable('schools')
       table.timestamps(true)
     })

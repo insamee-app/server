97d59e8a22fc2399ab27471cb096f75eddf4477a
Esteban S <esteban.soubiran@insa-cvl.fr>
2021-06-23 18:29:16 +0200

    refactor(todo): add some todo

--- start/routes.ts
+++ start/routes.ts
@@ -28,7 +28,7 @@ Route.get('/', async () => {
 Route.group(() => {
   Route.post('register', 'AuthController.register')
   Route.post('login', 'AuthController.login')
-  Route.post('logout', 'AuthController.logout')
+  Route.post('logout', 'AuthController.logout').middleware('auth')
 
   Route.post('verify/:email', 'AuthController.verifyEmail').as('verifyEmail')
   Route.post('resetPassword/:email', 'AuthController.resetPassword').as('resetPassword')

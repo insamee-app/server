2c30dda17d998f2ad2507268bd5969ec953cad22
Esteban S <esteban.soubiran@insa-cvl.fr>
2021-07-20 13:24:20 +0200

    feat(Tutorat): add registration migration

---
+++ database/migrations/1626780098482_registration_tutorat.ts
@@ -0,0 +1,23 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class RegistrationTutorat extends BaseSchema {
+  protected tableName = 'registration_tutorat'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.integer('user_id').unsigned().notNullable().references('id').inTable('users')
+      table.integer('tutorat_id').unsigned().notNullable().references('id').inTable('tutorats')
+
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}

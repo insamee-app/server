bc97c12f96e4c4238b8f322801e512c0ce64b2fc
Esteban S <esteban.soubiran@insa-cvl.fr>
2021-07-21 09:52:42 +0200

    feat(reports): create migrations

---
+++ database/migrations/1626853353887_associations_reports.ts
@@ -0,0 +1,35 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class AssociationsReports extends BaseSchema {
+  protected tableName = 'associations_reports'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table
+        .integer('reason_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('associations_reasons')
+      table.string('description').nullable()
+      table
+        .integer('association_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('associations')
+      table.integer('user_id').unsigned().notNullable().references('id').inTable('users')
+
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
---
+++ database/migrations/1626853356149_tutorats_reports.ts
@@ -0,0 +1,30 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class TutoratsReports extends BaseSchema {
+  protected tableName = 'tutorats_reports'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table
+        .integer('reason_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('tutorats_reasons')
+      table.string('description').nullable()
+      table.integer('tutorat_id').unsigned().notNullable().references('id').inTable('tutorats')
+      table.integer('user_id').unsigned().notNullable().references('id').inTable('users')
+
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}

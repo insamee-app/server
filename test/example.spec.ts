3bf76aaaf5076becf408129582022cd4ad2d4dbf
Esteban S <esteban.soubiran@insa-cvl.fr>
2021-08-13 21:36:34 +0200

    test: setup test runner

---
+++ test/example.spec.ts
@@ -0,0 +1,25 @@
+import test from 'japa'
+import supertest from 'supertest'
+import User from 'App/Models/User'
+
+const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}`
+
+test.group('Welcome', () => {
+  test('ensure home page works', async (assert) => {
+    /**
+     * Make request
+     */
+    const { body } = await supertest(BASE_URL).get('/').expect(200)
+
+    assert.deepEqual(body, { message: 'API from INSAMEE' })
+  })
+
+  test('ensure user password gets hashed during save', async (assert) => {
+    const user = new User()
+    user.email = 'virk@adonisjs.com'
+    user.password = 'secret'
+    await user.save()
+
+    assert.notEqual(user.password, 'secret')
+  })
+})

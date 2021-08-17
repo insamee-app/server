aee57f0af8088191a9e349ee1387f990186a97f6
Esteban S <esteban.soubiran@insa-cvl.fr>
2021-08-17 17:27:40 +0200

    feat(admin): show for reports

--- app/Controllers/Http/Api/v1/ReportsController.ts
+++ app/Controllers/Http/Api/v1/ReportsController.ts
@@ -4,6 +4,8 @@ import AssociationsReport from 'App/Models/AssociationsReport'
 import ProfilesReport from 'App/Models/ProfilesReport'
 import TutoratsReport from 'App/Models/TutoratsReport'
 import ReportValidator from 'App/Validators/ReportValidator'
+import { ModelQueryBuilderContract } from '@ioc:Adonis/Lucid/Orm'
+import NotFoundException from 'App/Exceptions/NotFoundException'
 
 export enum Resource {
   PROFILES = 'profiles',
@@ -47,5 +49,41 @@ export default class ReportsController {
       default:
         throw new Error(`Resource ${resource} not found`)
     }
+  }
+
+  public async show({ params, bouncer }: HttpContextContract) {
+    try {
+      await bouncer.with('ReportPolicy').authorize('view')
+    } catch (error) {
+      throw new ForbiddenException('Vous ne pouvez pas accéder à cette ressource')
+    }
+
+    const { resource, id } = params as { resource: Resource; id: number }
+
+    let reportQuery: ModelQueryBuilderContract<
+      typeof ProfilesReport | typeof TutoratsReport | typeof AssociationsReport,
+      ProfilesReport | TutoratsReport | AssociationsReport
+    >
+    switch (resource) {
+      case Resource.PROFILES:
+        reportQuery = ProfilesReport.query()
+        break
+      case Resource.TUTORATS:
+        reportQuery = TutoratsReport.query()
+        break
+      case Resource.ASSOCIATIONS:
+        reportQuery = AssociationsReport.query()
+        break
+      default:
+        throw new Error(`Resource ${resource} not found`)
+    }
+
+    let report: ProfilesReport | TutoratsReport | AssociationsReport
+    try {
+      report = await reportQuery.withTrashed().where('id', id).firstOrFail()
+    } catch (error) {
+      throw new NotFoundException('Signalement introuvable')
+    }
+    return report
   }
 }
